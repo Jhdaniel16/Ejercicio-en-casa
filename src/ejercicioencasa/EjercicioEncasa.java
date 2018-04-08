0ef85d3c7c425abacab0cdd5d4b9d3049cf3e333
Daniel's <Daniel's@LAPTOP-17EVJDNN>
2018-04-08 10:46:35 -0500

    Primer y segundo arreglo

--- src/ejercicioencasa/EjercicioEncasa.java
+++ src/ejercicioencasa/EjercicioEncasa.java
@@ -10,14 +10,21 @@ public class EjercicioEncasa {
     public static void main(String[] args) {
          Scanner entrada = new Scanner (System.in);
       int a[]; 
+      int b[];
    
       a = new int [10];
+      b = new int [10];
       
       System.out.println("Digite el primer arreglo: ");
         for (int i=0; i<10; i++){
            System.out.println((i+1)+". Digite un numero: ");
            a[i] = entrada.nextInt();
         }
+      System.out.println("Digite el segundo arreglo: ");
+        for (int i=0; i<10; i++){
+           System.out.println((i+1)+". Digite un numero: ");
+           b[i] = entrada.nextInt();
+        }  
     }
     
 }
