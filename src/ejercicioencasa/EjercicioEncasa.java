47acda9bff2e596d346eeb675286a11d9a0ab1a2
Daniel's <Daniel's@LAPTOP-17EVJDNN>
2018-04-08 10:50:30 -0500

    Mezcla del primer y segundo arreglo

--- src/ejercicioencasa/EjercicioEncasa.java
+++ src/ejercicioencasa/EjercicioEncasa.java
@@ -11,9 +11,11 @@ public class EjercicioEncasa {
          Scanner entrada = new Scanner (System.in);
       int a[]; 
       int b[];
+      int c[];
    
       a = new int [10];
       b = new int [10];
+      c = new int [20];
       
       System.out.println("Digite el primer arreglo: ");
         for (int i=0; i<10; i++){
@@ -25,6 +27,19 @@ public class EjercicioEncasa {
            System.out.println((i+1)+". Digite un numero: ");
            b[i] = entrada.nextInt();
         }  
+        int j=0;
+        for (int i=0; i<10; i++){
+         c[j] = a [i];
+         j++;
+         c[j] = b [i];
+         j++;
+        }
+        
+        System.out.print(" \n El tercer arreglo es: ");
+        for (int i=0; i<20; i++){
+           System.out.print(c[i]+ " "); 
+        }
+        System.out.println();
     }
     
 }
